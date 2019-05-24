6fc02e71638e56a847b1a9ac8bac4019bbc13735
sakis <zisoglou@hotmail.gr>
2019-05-25 01:05:42 +0200

    add english lg

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,14 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/4b27dbaa86c89227ecf85436b7945ba2/' + latitude + ',' + longitude +'?lang=el&units=si';
+    const url = 'https://api.darksky.net/forecast/4b27dbaa86c89227ecf85436b7945ba2/' + latitude + ',' + longitude +'?units=si';
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
